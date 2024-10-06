79a812f2a25609bdbd1d596aa7f59b083f36dd06
Matthew <matthew@loopspeed.co.uk>
2024-10-06 12:00:30 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     colors: {
-      white: "#F6F6F6",
-      "light-grey": "#CCC9C4",
-      "mid-grey": "#363430",
-      green: "#37FFA8",
-      cyan: "#37F3FF",
-      orange: "#E5A019",
-      "off-black": "#0C0B09",
-      black: "#000",
-      transparent: "transparent",
+      white: '#F6F6F6',
+      green: '#37FFA8',
+      cyan: '#37F3FF',
+      orange: '#E5A019',
+      'off-black': '#1E1B23',
+      black: '#000',
+      transparent: 'transparent',
     },
 
     extend: {
       fontFamily: {
-        sans: ["var(--font-inter)"],
+        sans: ['var(--font-inter)'],
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(circle, var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(circle, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
