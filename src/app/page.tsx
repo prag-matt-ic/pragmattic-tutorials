6b067456a13aa08533ee6eeed703b2308b77d75d
Matthew <matthew@loopspeed.co.uk>
2024-11-27 20:39:05 +0000

    Add under construction section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,10 @@ export default function HomePage() {
 
       <main className="pointer-events-none relative z-10 h-[300vh] w-full">
         <HomeHeader />
+
+        <section className="fixed bottom-4 left-4 rounded-md bg-black/20 p-3">
+          <h3 className="text-xl font-bold text-white">🚧 Under Construction 🚧</h3>
+        </section>
       </main>
     </>
   )
